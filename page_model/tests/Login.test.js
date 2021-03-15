55bbd2159d182724a56ac6b6dd75a1d47cc0ff9c
Marcos Sandoval <marcos@MBP-ZLJGH6-marcos.local>
2021-03-14 23:41:07 -0600

    6. Add multiple items to the shopping cart

--- page_model/tests/Login.test.js
+++ page_model/tests/Login.test.js
@@ -15,6 +15,7 @@ test('1. Login with a valid user', async t => {
 
 test('2. Login with an invalid user', async t => {
     await LoginPage.submitLoginForm(CREDENTIALS.INVALID_USER.USERNAME, CREDENTIALS.INVALID_USER.PASSWORD)
-    await t.expect(LoginPage.errorMessage.exists).ok()
-    await t.expect(LoginPage.errorMessage.innerText).eql('Epic sadface: Username and password do not match any user in this service')
+    await t
+        .expect(LoginPage.errorMessage.exists).ok()
+        .expect(LoginPage.errorMessage.innerText).eql('Epic sadface: Username and password do not match any user in this service')
 })
--- page_model/tests/Shopping.test.js
+++ page_model/tests/Shopping.test.js
@@ -15,7 +15,18 @@ test('4. Navigate to the shopping cart', async t => {
 })
 
 test('5. Add a single item to the shopping cart', async t => { 
-    await t.click(ProductPage.addBackpackButton)
-    await t.click(ProductPage.shoppingCartLink)
+    await t
+        .click(ProductPage.addBackpackButton)
+        .click(ProductPage.shoppingCartLink)
     await t.expect(ShoppingCartPage.backpackLink.exists).ok()
+})
+
+test('6. Add multiple items to the shopping cart', async t => { 
+    await t
+        .click(ProductPage.addBackpackButton)
+        .click(ProductPage.addTshirtButton)
+        .click(ProductPage.shoppingCartLink)
+    await t
+        .expect(ShoppingCartPage.backpackLink.exists).ok()
+        .expect(ShoppingCartPage.tshirtLink.exists).ok()
 })
